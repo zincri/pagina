eeda871d80ae3afb7e4ce2efd8755b2080515e03
Zincri Mendoza <zincri_mlz@hotmail.com>
2019-09-17 09:19:18 -0500

    Zincri
    Zincri

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php
-/*
+
 $array_usuarios = [
     "jhoana" => "admin",
     "zincri" => "client",
 ];
-*/
+
 $user = $_POST['user'];
 $pass = $_POST['pass'];
 
-if($user=="jhoana" && $pass=="123456"){
+if($user=="jhoana" && $pass=="123456" && $array_usuarios[$user]=="admin"){
     echo header("location:dashboard.html");
-}else if($user=="zincri" && $pass=="123"){
+}else if($user=="zincri" && $pass=="123" && $array_usuarios[$user]=="client"){
     echo header("location:principal.html");
 }else{
     echo header("location:login_h.html");
