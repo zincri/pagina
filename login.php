b24d7fd5873f28436eca530fad8f7018a2255869
Zincri Mendoza <zincri_mlz@hotmail.com>
2019-09-17 02:02:10 -0500

    Zincri
    Zincri

--- login.php
+++ login.php
@@ -1,5 +1,16 @@
 <?php
-
+/*
+$array_usuarios = [
+    "jhoana" => "admin",
+    "zincri" => "client",
+];
+*/
 $user = $_POST['user'];
-
-echo "Hola" ;
+if($user=="jhoana"){
+    echo header("location:dashboard.html");
+}else if($user=="zincri"){
+    echo header("location:principal.html");
+}else{
+    echo "error en la autenticacion";
+}
+?>
